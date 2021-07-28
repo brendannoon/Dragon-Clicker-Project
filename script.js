0623365d4e0d220b6fb7c00c4642fe6e953b7a5e
Brendan Noon <82846692+brendannoon@users.noreply.github.com>
2021-07-27 20:57:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-var clickNumber1 = 0;
-var clickNumber2 = 0;
+var forestDragon = dragon("forest");
+var waterDragon = dragon("water");
+var stoneDragon = dragon("stone");
+var sandDragon = dragon("sand");
+var iceDragon = dragon("ice");
 
-function addToD1ClickNumber(amount) {
-  clickNumber1 = clickNumber1 + amount;
-  document.getElementById("d1ClickNumber").innerHTML = clickNumber1;
-  
-}
 
-function addToD2ClickNumber(amount) {
-  clickNumber2 = clickNumber2 + amount;
-  document.getElementById("d2ClickNumber").innerHTML = clickNumber2;
-  
+function dragon(species) {
+    return {
+        category: `#${species} output`,
+        click:0,
+        addClick: function(clickNumber) {
+            this.click += clickNumber;
+            document.querySelector(this.category).innerText = this.click;
+        },
+        view: function() {
+            document.querySelector(".dragonCard").innerHTML = "";
+            document.querySelector(".dragonCard").innerHTML += `<div class="card" id="${species}" onclick="${species}Dragon.addClick(1)">
+              <h2>${species} dragon</h2>
+              <img src="https://cdn.glitch.com/8ff012a0-d643-4c08-b27e-7ce2744a4f17%2Fdragon_1f409.png?v=1626716716225" alt="${species} dragon" class="${species}" width="200">
+              <output>${this.click}</output>
+            </div>`
+        }
+    }
 }
